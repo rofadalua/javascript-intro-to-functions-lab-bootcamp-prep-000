a5988e6ae6d1096274ee81b0d6c66eeae9fc530b
ultimate-bell-1709 <rofadalua@hotmail.com>
2019-07-16 23:01:30 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
+describe('shout(string)', function shout (string) {
+   ('receives one argument and returns it in all caps', function() {
+    expect(shout('Hello')).toEqual('HELLO')
   })
 })
 
@@ -37,14 +37,14 @@ describe('logWhisper(string)', function() {
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+    expect(sayHiToGrandma('Hello')).toEqual("I can't hear you!")
   })
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+    expect(SAYHITOGRANDMA('HELLO')).toEqual("YES INDEED!")
   })
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+    expect(sayhitograndma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
